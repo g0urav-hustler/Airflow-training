05e40c27aff024ba79bd28803282aa3edf66ffde
g0urav-hustler <gouravchouhan966@gmail.com>
2024-06-25 17:39:21 +0530

    creating dag

--- weather_dag.py
+++ weather_dag.py
@@ -6,9 +6,14 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2024, 6, 25),
-    'email': [],
+    'email': ["myemail@gmail.com"],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 2,
     'retry_delay': timedelta(minutes=2)
 }
+
+with DAG("weather_dag",
+        default_args = default_args,
+        schedule_interval = "@daily",
+        catchup = False) as dag:
